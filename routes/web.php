8476076cdba73fab7e59a346ceb24f6fbf4f83bd
zasweted <zaswet@gmail.com>
2023-09-27 22:56:19 +0300

    added routs for new admin page

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin-master', function () {
-    return view('admin.admin-master');
-});
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
