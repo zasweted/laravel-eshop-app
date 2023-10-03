8e5ac1db684ba892d637fbb2ff6f7157a402a3bd
zasweted <zaswet@gmail.com>
2023-10-03 22:52:03 +0300

    added edit/update routes

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
     Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
     Route::get('/admin/profile', [AdminController::class, 'profile'])->name('admin.profile');
+    Route::get('/admin/profile/edit', [AdminController::class, 'editProfile'])->name('admin.profile.edit');
+    Route::post('/admin/profile/update', [AdminController::class, 'updateProfile'])->name('admin.profile.update');
 });
 
 Route::middleware('auth')->group(function () {
