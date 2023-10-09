fcbdd59769fdf472729d39e1d014ce5c2133b938
zasweted <zaswet@gmail.com>
2023-10-09 20:26:10 +0300

    added validation errors to view blade

--- resources/views/admin/admin-profile-password-change.blade.php
+++ resources/views/admin/admin-profile-password-change.blade.php
@@ -12,6 +12,7 @@
                         <h4 class="card-title text-center">Hi {{ $userData->username }}, Lets change your password!</h4>
 
                         <div class="p-3">
+                           
                             <form class="form-horizontal mt-3" method="POST"
                                 action="{{ route('admin.profile.passw-update') }}">
                                 @csrf
@@ -21,37 +22,37 @@
                                     <label for="old_password" class="col-sm-2 ">Old Password</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" id="old_password" type="password"
-                                            name="old_password" required autofocus autocomplete="current-password"
+                                            name="old_password"  autofocus autocomplete="current-password"
                                             placeholder="Password" />
+                                            @error('old_password')
+                                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                                            @enderror
                                     </div>
                                 </div>
-                                @error('password')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
 
                                 <!-- New pass -->
                                 <div class="mb-2 row">
                                     <label for="new_password" class="col-sm-2">New password</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" id="new_password" type="password"
-                                            name="new_password" required autofocus placeholder="New Password" />
+                                            name="new_password"  autofocus placeholder="New Password" />
+                                            @error('new_password')
+                                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                                            @enderror
                                     </div>
                                 </div>
-                                @error('new_password')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
 
                                 <!-- Repeat New Pass -->
                                 <div class="mb-2 row">
                                     <label for="repeat_password" class="col-sm-2">Repeat Password</label>
                                     <div class="col-sm-10">
-                                        <input class="form-control" id="repeat_password" type="password" name="repeat_password"
-                                            required placeholder="Repeat Password" />
+                                        <input class="form-control" id="repeat_password" type="password"
+                                            name="repeat_password"  placeholder="Repeat Password" />
+                                            @error('repeat_password')
+                                            <div class="alert alert-danger mt-2">{{ $message }}</div>
+                                            @enderror
                                     </div>
                                 </div>
-                                @error('email')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                                @enderror
 
                                 <div class="form-group text-left row mt-3 pt-1">
                                     <div class="col-12">
